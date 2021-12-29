256d735601b75ace8126337a9a55f2bd4975ebd8
Dinar <doouw@mail.ru>
2021-12-29 12:09:32 +0500

    Fixed proc GetAllProductById

--- CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllProductInfoById.sql
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllProductInfoById.sql
@@ -1,10 +1,12 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllProductInfoById]
 	@Id integer
 AS
-	SELECT P.Name, C.Mark, C.Amount, O.Date, O.CustomerID from [dbo].[Product] as P
-join [dbo].[Check] as C
+	SELECT P.Name, C.Mark, C.Amount, O.Date, O.CustomerID, G.Name from [dbo].[Product] as P
+left join [dbo].[Check] as C
 on P.Id=C.ProductId
-join [dbo].[Order] as O
+left join [dbo].[Order] as O
 on C.OrderId = O.ID
+left join [dbo].[Group] as G
+on P.GroupId=G.Id
 where P.Id=@Id
 RETURN 0
