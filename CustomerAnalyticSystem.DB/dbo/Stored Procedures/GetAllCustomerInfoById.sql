870551d7ed62abe29207fb041936d3e1927ab6b6
AzarovRoman <azarovrom9215@gmail.com>
2022-01-03 20:47:50 +0300

    Add GetCustomerInfoById

---
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllCustomerInfoById.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetAllCustomerInfoById]
+  @Id integer
+as
+  select * from Customer as C
+  inner join Contact as Cont
+  on C.Id = Cont.CustomerId
+  inner join ContactType as CT
+  on Cont.ContactTypeId = CT.Id
+  inner join Comment as Com
+  on C.Id = Com.CustomerId
+  where C.Id = @Id
+RETURN 0
