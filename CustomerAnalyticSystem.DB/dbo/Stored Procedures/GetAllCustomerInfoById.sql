19fb271d1ea053c4cb23da39dfbad013cfb6647c
AzarovRoman <azarovrom9215@gmail.com>
2022-01-04 11:06:08 +0300

    Fix duplicate (?)

--- CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllCustomerInfoById.sql
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllCustomerInfoById.sql
@@ -1,12 +1,13 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllCustomerInfoById]
   @Id integer
 as
-  select * from Customer as C
+  select C.Id, C.FirstName, C.LastName, CT.Name, Cont.Value,  null from Customer as C
   inner join Contact as Cont
   on C.Id = Cont.CustomerId
   inner join ContactType as CT
   on Cont.ContactTypeId = CT.Id
-  inner join Comment as Com
-  on C.Id = Com.CustomerId
   where C.Id = @Id
-RETURN 0
+  union all
+  select null, null, null, null, null, Com.Text from Comment as Com
+  where Com.CustomerId = @Id
+RETURN @Id
