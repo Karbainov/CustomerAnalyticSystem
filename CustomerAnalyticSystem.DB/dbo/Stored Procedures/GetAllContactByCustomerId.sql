e3ca232a2669e69df19b2dd5451d7295d8645e54
AzarovRoman <azarovrom9215@gmail.com>
2022-01-10 20:34:03 +0300

    add GetAllContactByCustomerId and GetAllCommentByCustomerId

---
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllContactByCustomerId.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GetAllContactByCustomerId]
+	@Id integer
+as
+	select * from Contact as cont
+	inner join ContactType as CT
+	on cont.ContactTypeId = CT.Id
+	where CustomerId = @Id
+RETURN @Id
