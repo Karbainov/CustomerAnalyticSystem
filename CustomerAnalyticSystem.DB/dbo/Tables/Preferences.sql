a422368c4d63b8a2ee7b9d9839eea02ed8949799
Alyona <alianakass@icloud.com>
2022-01-10 19:16:48 +0300

    Get all preferences by customerId and bagfix

--- CustomerAnalyticSystem.DB/dbo/Tables/Preferences.sql
+++ CustomerAnalyticSystem.DB/dbo/Tables/Preferences.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[Preferences]
 (
 	ID integer identity primary key,
-	ProductID integer,
-	CustumerID integer,
+	[ProductId] integer,
+	[CustomerId] integer,
 	TagID integer,
 	GroupID integer,
 	IsInterested bit,
-	Foreign key (ProductID) references [Product] (ID),
-	Foreign key (CustumerID) references [Customer] (ID),
+	Foreign key ([ProductId]) references [Product] (ID),
+	Foreign key ([CustomerId]) references [Customer] (ID),
 	Foreign key (TagID) references [Tag] (ID),
 	Foreign key (GroupID) references [Group] (ID)
 )
