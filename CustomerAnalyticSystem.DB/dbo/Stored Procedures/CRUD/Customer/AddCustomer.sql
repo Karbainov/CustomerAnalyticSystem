4a1fdde06abfde3c09aeff44e2a2d3c69e1f1b00
AzarovRoman <azarovrom9215@gmail.com>
2022-01-10 17:46:51 +0300

    add all crud

---
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/CRUD/Customer/AddCustomer.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[AddCustomer]
+  @LastName nvarchar(50),
+  @FirstName nvarchar(50),
+  @TypeId integer
+as
+  insert into dbo.Customer (LastName, FirstName, TypeId)
+  values 
+  (@LastName, @FirstName, @TypeId)
