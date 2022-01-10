6174a93d0ff7d67d90b92aaab2904ed39fcbcf7c
Alyona <alianakass@icloud.com>
2022-01-10 19:09:19 +0300

    Get all products by tag

---
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllProductsByTag.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[GetAllProductsByTag]
+	@Id integer
+	as
+	select P.[Name] from dbo.[Product] as P
+	inner join Product_Tag as PT
+	on PT.ProductId = P.Id and PT.TagId = @Id
+RETURN 0
