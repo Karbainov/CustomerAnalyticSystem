85981a4f79c8061ceacec68c135e8ed70ed25b82
Artur Khamatnurov <arturkh.116@gmail.com>
2022-01-11 22:05:19 +0300

    CRUD for Tag

---
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/CRUD/Product_Tag/GetAllProduct_TagById.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[GetAllProduct_TagById]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+RETURN 0
---
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/CRUD/Tag/DeleteTagById.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[DeleteTagById]
+	@Id integer
+AS
+	delete from Tag 
+	where Id = @Id
+	return @Id
