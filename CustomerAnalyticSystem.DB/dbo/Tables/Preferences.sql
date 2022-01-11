919aa2d97651d9ba6dd29116d37adef4005a5312
Artur Khamatnurov <arturkh.116@gmail.com>
2022-01-11 22:30:54 +0300

    Naming fixes

--- CustomerAnalyticSystem.DB/dbo/Tables/Preferences.sql
+++ CustomerAnalyticSystem.DB/dbo/Tables/Preferences.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[Preferences]
 (
-	ID integer identity primary key,
+	Id integer identity primary key,
 	[ProductId] integer NOT NULL,
 	[CustomerId] integer NOT NULL,
-	TagID integer NOT NULL,
-	GroupID integer NOT NULL,
+	TagId integer NOT NULL,
+	GroupId integer NOT NULL,
 	IsInterested bit NOT NULL,
-	Foreign key ([ProductId]) references [Product] (ID),
-	Foreign key ([CustomerId]) references [Customer] (ID),
-	Foreign key (TagID) references [Tag] (ID),
-	Foreign key (GroupID) references [Group] (ID)
+	Foreign key ([ProductId]) references [Product] (Id),
+	Foreign key ([CustomerId]) references [Customer] (Id),
+	Foreign key (TagId) references [Tag] (Id),
+	Foreign key (GroupId) references [Group] (Id)
 )
