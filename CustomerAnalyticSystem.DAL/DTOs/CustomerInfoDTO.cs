24a48dba200e332845058928cceca65be4b52e2f
AzarovRoman <azarovrom9215@gmail.com>
2022-01-12 15:17:22 +0300

    Add CustomerInfoDTO

--- CustomerAnalyticSystem.DAL/DTOs/CustomerInfoDTO.cs
+++ CustomerAnalyticSystem.DAL/DTOs/CustomerInfoDTO.cs
@@ -8,5 +8,13 @@ namespace CustomerAnalyticSystem.DAL.DTOs
 {
     internal class CustomerInfoDTO
     {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        int TypeId { get; set; }
+
+        List<CommentDTO> Comments;
+
+        List<ContactsDTO> Contacts;
     }
 }
