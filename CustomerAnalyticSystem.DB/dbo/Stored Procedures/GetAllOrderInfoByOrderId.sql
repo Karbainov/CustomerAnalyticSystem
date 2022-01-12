8e2177d43110fbff01e25c331c009e81d63a0a9e
Dinar <doouw@mail.ru>
2022-01-12 13:44:54 +0500

    8

--- CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllOrderInfoByOrderId.sql
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllOrderInfoByOrderId.sql
@@ -1,6 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllOrderInfoByOrderId]
 @Id integer
 AS
-	SELECT C.Id, C.ProductId, C.OrderId, C.Amount, C.Mark FROM [dbo].[Check] AS C
+	SELECT Cust.Id as "CustomerId",C.Id, C.ProductId, C.OrderId, C.Amount, C.Mark FROM [dbo].[Check] AS C
+	join [dbo].[Order] as O
+	on C.OrderId = O.ID
+	join [dbo].[Customer] as Cust
+	on Cust.Id = O.CustomerID
 	WHERE  C.OrderId = @Id
 RETURN 0
