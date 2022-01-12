a4d4162df21a197f248ed33a7519288c48f00345
Yana <Yana.ilina.1999@list.ru>
2022-01-12 21:14:20 +0300

    plus get all grades by customerid

--- CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllGradesByCustomerId.sql
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetAllGradesByCustomerId.sql
@@ -1,9 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[GetAllGradesByCustomerId]
 	@Id integer
 as
-  select C.Name, C.Age, P.Name, P.Description, G.Value, Ch.Amount, Ch.Mark from Grade as G
+  select C.FirstName, P.Name, P.Description, G.Value, Ch.Amount, Ch.Mark from Grade as G
   left join [dbo].[Customer] as C
-  on C.Id=G.CustomerId
+  on C.Id=G.CustumerId
   left join [dbo].[Product] as P
   on P.Id=G.ProductId
   left join [dbo].[Check] as Ch
