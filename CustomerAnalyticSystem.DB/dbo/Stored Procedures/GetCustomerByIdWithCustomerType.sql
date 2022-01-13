ed1629f39b1e8c5849b3e60beb8a18ed7ec02c9b
AzarovRoman <azarovrom9215@gmail.com>
2022-01-13 23:45:36 +0300

    review

--- CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetCustomerByIdWithCustomerType.sql
+++ CustomerAnalyticSystem.DB/dbo/Stored Procedures/GetCustomerByIdWithCustomerType.sql
@@ -1,7 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[GetCustomerByIdWithCustomerType]
 	@id integer
 AS
-	select C.Id, C.FirstName, C.LastName, C.TypeId, CT.Name from [dbo].[Customer] as C
+	select C.Id, C.FirstName, C.LastName, C.TypeId, CT.[Name] from [dbo].[Customer] as C
 	left join [dbo].[CustomerType] as CT
 	on C.TypeId = CT.Id
 	where C.Id = @id
