abbd1981a384eed87ca67642520fd9b3308d29eb
Artur Khamatnurov <arturkh.116@gmail.com>
2022-01-13 14:11:51 +0300

    Get all marks of tags by CustomerId

---
+++ CustomerAnalyticSystem.DAL/CustumerInfoService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerAnalyticSystem.DAL.DTOs;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+
+namespace CustomerAnalyticSystem.DAL
+{
+    public class CustumerInfoService
+    {
+        public List <TagMarksDTO> GetAllMarksOfTagByCustomerId (int id)
+        {
+            List<TagMarksDTO> tagMarksDTOs = new List<TagMarksDTO>();
+            string query = "GetAllTagsWithMarksByCustomerId";
+            string connectionString = ConnectionString.Connection;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                tagMarksDTOs = connection.Query<TagMarksDTO>(query, param:id).ToList() ;
+            }
+            return tagMarksDTOs;
+        }
+    }
+}
--- TempProject/Program.cs
+++ TempProject/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using CustomerAnalyticSystem.DAL;
+using CustomerAnalyticSystem.DAL.DTOs;
+
 
 namespace TempProject
 {
@@ -11,17 +13,21 @@ namespace TempProject
     {
         static void Main(string[] args)
         {
-            List<CustomerAnalyticSystem.DAL.DTOs.ProductDTO> products;
-            string query = "EXEC GetAllProduct";
-            string connectionString = ConnectionString.Connection;
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-               products = connection.Query<CustomerAnalyticSystem.DAL.DTOs.ProductDTO>(query).ToList();
-            }
-            foreach (var p in products)
-            {
-                Console.WriteLine((CustomerAnalyticSystem.DAL.DTOs.ProductDTO)p);
-            }
+            //List<CustomerAnalyticSystem.DAL.DTOs.ProductDTO> products;
+            //string query = "EXEC GetAllProduct";
+            //string connectionString = ConnectionString.Connection;
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //   products = connection.Query<CustomerAnalyticSystem.DAL.DTOs.ProductDTO>(query).ToList();
+            //}
+            //foreach (var p in products)
+            //{
+            //    Console.WriteLine((CustomerAnalyticSystem.DAL.DTOs.ProductDTO)p);
+            //}
+            CustumerInfoService custumerInfoService = new CustumerInfoService();
+            List<TagMarksDTO> tempList = new List<TagMarksDTO>();
+            tempList = custumerInfoService.GetAllMarksOfTagByCustomerId(1);
+
         }
     }
 }
