059a73b29ddd15bc77b9c1b01a433384cfb7448f
Yana <Yana.ilina.1999@list.ru>
2022-01-14 17:30:25 +0300

    Grade Dapper

---
+++ CustomerAnalyticSystem.DAL/GradeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using CustomerAnalyticSystem.DAL;
+using CustomerAnalyticSystem.DAL.DTOs;
+using System.Data;
+
+namespace CustomerAnalyticSystem.DAL
+{
+    public class GradeRepository
+    {
+        public List<GradeDTO> GetAllGrades()
+        {
+            List<GradeDTO> grades = new List<GradeDTO>();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString.Connection))
+            {
+                grades = connection.Query<GradeDTO>(Querys.GetAllGrades).ToList();
+            }
+            return grades;
+        }
+    }
+}
--- TempProject/Program.cs
+++ TempProject/Program.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using Dapper;
-using Microsoft.Data.SqlClient;
+﻿using CustomerAnalyticSystem.DAL.DTOs;
+using CustomerAnalyticSystem.DAL;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using CustomerAnalyticSystem.DAL.DTOs;
-using CustomerAnalyticSystem.DAL;
-using CustomerAnalyticSystem.DAL.DTOs;
-using CustomerAnalyticSystem.DAL;
-
 
 namespace TempProject
 {
@@ -15,41 +9,46 @@ namespace TempProject
     {
         static void Main(string[] args)
         {
-            //CustomerService customerDapper = new CustomerService();
-            //List<CustomerDTO> c = customerDapper.GetAllCustomerService();
+            ////CustomerService customerDapper = new CustomerService();
+            ////List<CustomerDTO> c = customerDapper.GetAllCustomerService();
 
-            //foreach (CustomerDTO cust in c)
-            //{
-            //    Console.WriteLine(cust);
-            //}
-            CustomerService temp = new();
-            CustomerInfoDTO qqq = 
-            temp.GetCustomerInfoService(1);
-            //List<AllOrderInfoByOrderId> products;
-            //string query = "EXEC GetAllOrderInfoByOrderId 1";
-            //string connectionString = @"Data Source=DESKTOP-16PSAEB;Initial Catalog=CreateAnalyticSystem;Integrated Security=True;Persist Security Info=False; Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //   products = connection.Query<AllOrderInfoByOrderId>(query).ToList();
-            //}
-            //foreach (var p in (List<AllOrderInfoByOrderId>)products)
-            //{
-            //    Console.WriteLine((AllOrderInfoByOrderId)p);
-            //}
-            AllOrderInfoByOrderId kekis = new();
-            FillOrderInfo test = new();
-            kekis = test.FillOrderInfoByOrderId(1);
-            //List<CustomerAnalyticSystem.DAL.DTOs.ProductDTO> products;
-            //string query = "EXEC GetAllProduct";
-            //string connectionString = ConnectionString.Connection;
-            //using (SqlConnection connection = new SqlConnection(connectionString))
-            //{
-            //   products = connection.Query<CustomerAnalyticSystem.DAL.DTOs.ProductDTO>(query).ToList();
-            //}
-            //foreach (var p in products)
-            //{
-            //    Console.WriteLine((ProductDTO)p);
-            //}
+            ////foreach (CustomerDTO cust in c)
+            ////{
+            ////    Console.WriteLine(cust);
+            ////}
+            //CustomerService temp = new();
+            //CustomerInfoDTO qqq = 
+            //temp.GetCustomerInfoService(1);
+            ////List<AllOrderInfoByOrderId> products;
+            ////string query = "EXEC GetAllOrderInfoByOrderId 1";
+            ////string connectionString = @"Data Source=DESKTOP-16PSAEB;Initial Catalog=CreateAnalyticSystem;Integrated Security=True;Persist Security Info=False; Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
+            ////using (SqlConnection connection = new SqlConnection(connectionString))
+            ////{
+            ////   products = connection.Query<AllOrderInfoByOrderId>(query).ToList();
+            ////}
+            ////foreach (var p in (List<AllOrderInfoByOrderId>)products)
+            ////{
+            ////    Console.WriteLine((AllOrderInfoByOrderId)p);
+            ////}
+            //AllOrderInfoByOrderId kekis = new();
+            //FillOrderInfo test = new();
+            //kekis = test.FillOrderInfoByOrderId(1);
+            ////List<CustomerAnalyticSystem.DAL.DTOs.ProductDTO> products;
+            ////string query = "EXEC GetAllProduct";
+            ////string connectionString = ConnectionString.Connection;
+            ////using (SqlConnection connection = new SqlConnection(connectionString))
+            ////{
+            ////   products = connection.Query<CustomerAnalyticSystem.DAL.DTOs.ProductDTO>(query).ToList();
+            ////}
+            ////foreach (var p in products)
+            ////{
+            ////    Console.WriteLine((ProductDTO)p);
+            ////}
+
+            List<GradeDTO> grades = new List<GradeDTO>();
+            GradeRepository Repository = new();
+            grades = Repository.GetAllGrades();
+
         }
     }
 }
