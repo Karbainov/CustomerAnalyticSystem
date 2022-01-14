0f9b61c3894b3edd40e18fb9691851418ea246d9
Yana <Yana.ilina.1999@list.ru>
2022-01-14 23:16:49 +0300

    Add Dupper for OrderCRUD

---
+++ CustomerAnalyticSystem.DAL/OrderRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using CustomerAnalyticSystem.DAL;
+using CustomerAnalyticSystem.DAL.DTOs;
+using System.Data;
+
+
+
+
+
+namespace CustomerAnalyticSystem.DAL
+{
+   public class OrderRepository
+    {
+        public List<OrderDTO> GetAllOrders()
+        {
+            List<OrderDTO> orders = new List<OrderDTO>();
+            using(SqlConnection connection = new SqlConnection(ConnectionString.Connection))
+            {
+                orders = connection.Query<OrderDTO>(Querys.GetAllOrders).ToList();
+            }
+            return orders;
+        }
+
+        public OrderDTO GetOrderById(int id)
+        {
+            OrderDTO order;
+            using(SqlConnection connection = new SqlConnection(ConnectionString.Connection))
+            {
+                order = connection.QuerySingle<OrderDTO>(Querys.GetOrderById, new { id }
+               , commandType: CommandType.StoredProcedure);
+            }
+            return order;
+        }
+    }
+}
