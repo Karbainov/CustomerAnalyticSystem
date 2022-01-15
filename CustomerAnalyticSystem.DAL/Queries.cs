556e238460a86dbc6263556eed9509e1e53f7f6f
Artur Khamatnurov <arturkh.116@gmail.com>
2022-01-15 15:04:35 +0300

    Add Tag and Product_Tag consts to Queries.cs

--- CustomerAnalyticSystem.DAL/Queries.cs
+++ CustomerAnalyticSystem.DAL/Queries.cs
@@ -15,5 +15,17 @@ namespace CustomerAnalyticSystem.DAL
         public const string GetCustomerByIdWithCustomerType = "GetCustomerByIdWithCustomerType";
         public const string GetAllOrderInfoByOrderId = "GetAllOrderInfoByOrderId";
         public const string GetAllTagsWithMarksByCustomerId = "GetAllTagsWithMarksByCustomerId";
+
+        public const string AddProduct_Tag = "AddProduct_Tag";
+        public const string DeleteProduct_Tag = "DeleteProduct_Tag";
+        public const string GetAllProduct_Tag = "GetAllProduct_Tag";
+        public const string GetProduct_TagById = "GetProduct_TagById";
+        public const string UpdateProduct_TagById = "UpdateProduct_TagById";
+
+        public const string AddTag = "AddTag";
+        public const string DeleteTagById = "DeleteTagById";
+        public const string GetAllTags = "GetAllTags";
+        public const string GetTagById = "GetTagById";
+        public const string UpdateTagById = "UpdateTagById";
     }
 }
