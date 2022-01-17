1397be62ba89e8c83e5f4945d507522340d62c6f
Dinar <doouw@mail.ru>
2022-01-17 17:41:49 +0500

    Added status

--- CustomerAnalyticSystem.BLL/Models/OrderInfoByOrderIdModel.cs
+++ CustomerAnalyticSystem.BLL/Models/OrderInfoByOrderIdModel.cs
@@ -12,6 +12,7 @@ namespace CustomerAnalyticSystem.BLL.Models
         public int CustomerId { get; set; }
         public int OrderId { get; set; }
         public int StatusId { get; set; }
+        public string Status { get; set; }
         public List<CheckBaseModel> Items { get; set; }
     }
 }
