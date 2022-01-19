e33d7d6c82ebad169aacfac23e3556cfd5270b17
Alyona <alianakass@icloud.com>
2022-01-19 15:42:59 +0300

    MAPBASETAG

--- CustomerAnalyticSystem.BLL/Services/ProductTagGroupService.cs
+++ CustomerAnalyticSystem.BLL/Services/ProductTagGroupService.cs
@@ -11,5 +11,13 @@ namespace CustomerAnalyticSystem.BLL.Services
 {
     public class ProductTagGroupService
     {
+        public List <TagModel> GetTag()
+        {
+            MrMappi map = new();
+            var service = new ProductTagGroupRepository();
+            var dto = service.GetAllTags();
+            List<TagModel> result = map.MapFromTagDTOToTagModel(dto);
+            return result;
+        }
     }
 }
