0710a7a004335931baba423588d03814a7993373
Alyona <alianakass@icloud.com>
2022-01-19 19:07:44 +0300

    Map contact and contactType Delete and Update

--- CustomerAnalyticSystem.BLL/Services/ContactTypeContactService.cs
+++ CustomerAnalyticSystem.BLL/Services/ContactTypeContactService.cs
@@ -11,5 +11,30 @@ namespace CustomerAnalyticSystem.BLL.Services
 {
     public class ContactTypeContactService
     {
+        public void UpdateContact (int id, int customerId, int contactType, string value, string name)
+        {
+            ContactTypeContactRepository rep = new ContactTypeContactRepository();
+            rep.UpdateContact(id, customerId, contactType, value);
+        }
+
+        public void DeleteContact (int id)
+        {
+            ContactTypeContactRepository rep = new ContactTypeContactRepository();
+            rep.DeleteContact(id);
+        }
+        
+        public void UpdateContactType (int id, string name)
+        {
+            ContactTypeContactRepository rep = new ContactTypeContactRepository();
+            rep.UpdateContactType(id, name);
+        }
+
+        public void DeleteContactType(int id)
+        {
+            ContactTypeContactRepository rep = new ContactTypeContactRepository();
+            rep.DeleteContactType(id);
+        }
+
+
     }
 }
