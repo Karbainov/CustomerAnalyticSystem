1f658178fd12c19bd613f54a666061a844a96ad2
Dinar <doouw@mail.ru>
2022-01-19 22:27:38 +0500

    Minor fixes

--- CustomerAnalyticSystem.DB/dbo/Tables/Grade.sql
+++ CustomerAnalyticSystem.DB/dbo/Tables/Grade.sql
@@ -3,7 +3,7 @@
 	[Id] integer identity primary key,
 	[ProductId] integer NOT NULL,
 	[CustomerId] integer NOT NULL,
-	[Value] integer NOT NULL,
+	[Value] integer NULL,
 	Foreign key (ProductID) references [Product] (ID),
 	Foreign key (CustomerID) references [Customer] (ID)
 )
