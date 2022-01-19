f0815fc91791359cea7500c1e857e843d43d3aec
AzarovRoman <azarovrom9215@gmail.com>
2022-01-19 10:52:48 +0300

    some fix

--- CustomerAnalyticSystem.BLL/Services/CustomerService.cs
+++ CustomerAnalyticSystem.BLL/Services/CustomerService.cs
@@ -30,20 +30,10 @@ namespace CustomerAnalyticSystem.BLL
             return  map.MapListCustomerDTOToListCustomerModel(customers);
         }
 
-        //public CustomerModel GetCustomerModel(int id)
-        //{
-        //    CustomerTypeCustomerCommentRepository rep = new CustomerTypeCustomerCommentRepository();
-        //    CustomerInfoDTO DTO = rep.GetCustomerInfoService(id);
-        //    MyMapper map = new MyMapper();
-        //    CustomerModel result = map.MapCustomerInfoDTOToCustomerModel(DTO);
-
-        //    return result;
-        //}
-
-        //public void UpdateCustomer(int id, string firstName, string lastName, int TypeId = 1)
-        //{
-        //    CustomerTypeCustomerCommentRepository rep = new CustomerTypeCustomerCommentRepository();
-        //    rep.UpdateCustomerById(id, firstName, lastName, TypeId);
-        //}
+        public void UpdateCustomer(int id, string firstName, string lastName, int TypeId = 1)
+        {
+            CustomerTypeCustomerCommentRepository rep = new CustomerTypeCustomerCommentRepository();
+            rep.UpdateCustomerById(id, firstName, lastName, TypeId);
+        }
     }
 }
