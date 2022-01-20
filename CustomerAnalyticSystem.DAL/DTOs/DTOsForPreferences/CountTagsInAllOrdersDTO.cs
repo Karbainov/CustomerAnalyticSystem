4fcaac107e6ad8771c9cfc1e41194c27aa56faf7
Dinar <doouw@mail.ru>
2022-01-21 04:26:59 +0500

    DTOS/prefs Count2files, 2 storedProcedures

---
+++ CustomerAnalyticSystem.DAL/DTOs/DTOsForPreferences/CountTagsInAllOrdersDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAnalyticSystem.DAL.DTOs.DTOsForPreferences
+{
+    public class CountTagsInAllOrdersDTO
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+        public int Number { get; set; }
+    }
+}
